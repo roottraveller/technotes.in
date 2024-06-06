886be8a0d909d4e5eec88694a0e9d1dd30dc58eb
rishi <rishi.nit.iim@gmail.com>
2024-06-06 20:54:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>technote.in</h1>
       </header>
+      <div className="Main-container">
+        <div className="Side-panel">
+          <ul>
+            <li>heading-text1</li>
+            <li>heading-text2</li>
+            <li>heading-text3</li>
+          </ul>
+        </div>
+        <div className="Main-content">
+          {/* Your main text and image section goes here */}
+        </div>
+      </div>
     </div>
   );
 }
